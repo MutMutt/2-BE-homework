f58cffb808ce010aaadf60f9e895956fea562b6d
MutMutt <62054882+MutMutt@users.noreply.github.com>
2021-03-02 17:22:43 +0000

    full

---
+++ Data/Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+        public DbSet<Values> Values { get; set; }
+    }
+}
+
